a7fa3ba80bdc066aeade3cfcba240bb6bf7e91dd
Fatman <nobody@nobody.co.com>
2017-01-08 01:38:48 -0800

    Updated Error Handling debug message

--- confirm_request.py
+++ confirm_request.py
@@ -5,8 +5,10 @@ import urllib2
 import re
 from xml.etree import ElementTree as etree
 from podcast import *
+import alexa as a
 
 def get_confirm_answer(confirm):
+	print("Confirming Answer")
 	if re.search(r'(?i)yes|yeah|oh kay|ok|sure|ya',confirm):
 		answer = "YES"
 	else:
@@ -14,17 +16,27 @@ def get_confirm_answer(confirm):
 	return answer
 
 def confirmation_play_recent_podcast(session_attr):
-	print(session)
+	try:
+		print("Send Most Recent Podcast")
+		response = build_response(session_attr["stream"],None)
+		print(session)
+	except:
+		print("Handle Exception confirmation_play_recent_podcast")
+	return response
 
 def handle_confirm_request(intent,session):
-	slots = intent_request['slots']
-	confirm = slots["confirm"]
+	slots = intent['slots']
+	confirm = slots["confirm"]["value"]
+	confirm_answer = get_confirm_answer(confirm)
 	if 'prevIntent' in session["attributes"]:
 		prevIntent = session["attributes"]["prevIntent"]
 		if prevIntent == "ListRecentPodcast":
-			response = confirmation_play_recent_podcast(session["attributes"])
-			return response
+			if confirm_answer == "YES":
+				response = confirmation_play_recent_podcast(session["attributes"])
+				return response 
+			else:
+				response = a.on_session_ended(intent,session)
 		else:
-			print("Handle Exception")
+			print("Handle Exception handle_confirm_request::PrevIntent YES")
 	else:
-		print("Handle Exception")
+		print("Handle Exception handle_confirm_request::PrevIntent NO")
